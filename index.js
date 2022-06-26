2ff825483533edad47f2f3068c438fecd8d98f35
Edgar <edgaras993@gmail.com>
2022-06-26 16:33:03 -0400

    my first commit

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(cors());
 
 app.use("/api", apiRouter);
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
