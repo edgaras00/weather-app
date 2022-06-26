add8cee6c9ea775370e1cd41666212304969441c
Edgar <edgaras993@gmail.com>
2022-06-26 15:59:39 -0400

    deploy

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.use(cors());
 
 app.use("/api", apiRouter);
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, ".client/build")));
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, ".client/build", "index.html"));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
