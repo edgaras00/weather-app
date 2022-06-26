1aaba9e716b156f8992a354c0ba37b30df13eb16
Edgar <edgaras993@gmail.com>
2022-06-26 15:44:26 -0400

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const apiRouter = require("./api/routes/apiRoutes");
+const apiRouter = require("./routes/apiRoutes");
 require("dotenv").config();
 
 const app = express();
@@ -10,9 +10,6 @@ app.use(cors());
 app.use("/api", apiRouter);
 
 app.use(express.static(path.join(__dirname, "/client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
